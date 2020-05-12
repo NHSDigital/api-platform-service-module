626021ce161330aa87cd155cd628495d946a510e
Laurence Smith <laurence.smith@airelogic.com>
2020-05-12 18:10:29 +0100

    APM-855 HOTFIX Set public visibility

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "apigee_product" "product" {
   quota_time_unit = "minute"
 
   attributes = {
-    access = length(regexall("prod", var.apigee_environment)) > 0 ? "internal" : "public",
+    access = "public",
     ratelimit = "5ps"
   }
 
